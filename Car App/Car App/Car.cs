03ae267ff125888e26065b826d5328bd938c5820
20074694 <20074694@tafe.wa.edu.au>
2022-05-10 17:26:22 +0800

    some work

---
+++ Car App/Car App/Car.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Car_App
+{
+    class Car
+    {
+        int regoNumber;
+        Make carMake;
+        Model carModel;
+        private int carYear;
+        private float price;
+        string photoFilename;
+
+        public int RegoNumber
+        {
+            get
+            {
+                return regoNumber;
+            }
+            set
+            {
+                regoNumber = value;
+            }
+        }
+
+        public Make CarMake
+        {
+            get
+            {
+                return carMake;
+            }
+            set
+            {
+                carMake = value;
+            }
+        }
+        
+        public Model CarModel
+        {
+            get
+            {
+                return carModel;
+            }
+            set
+            {
+                Type t = value.GetType();
+                if (t == typeof(Model)){
+                    carModel = value;
+                }
+                else
+                {
+                    carModel = Model.Sudan;
+                }
+            }
+        }
+
+        public int CarYear
+        {
+            get
+            {
+                return carYear;
+            }
+            set
+            {
+                if(value >= 1900)
+                {
+                    carYear = value;
+                }
+                else
+                {
+                    carYear = 1900;
+                }
+            }
+        }
+
+        public float Price
+        {
+            get
+            {
+                return price;
+            }
+            set
+            {
+                if(value < 0)
+                {
+                    price = 0;
+                }
+                else
+                {
+                    Math.Round(value, 2);
+                    price = value;
+                }
+                
+            }
+        }
+
+        
+        /// <summary>
+        /// Default Constructor - Creates new Car object with default values.
+        /// </summary>
+        public Car()
+        {
+            regoNumber = 1000;
+            carMake = Make.White;
+            carModel = Model.Sudan;
+            CarYear = 1950;
+            Price = 20000.00f;
+            photoFilename = "";
+        }
+
+        /// <summary>
+        /// Copy Constructor - Creates new Car object with values set to Car input.
+        /// </summary>
+        public Car(Car car)
+        {
+            regoNumber = car.regoNumber;
+            carMake = car.carMake;
+            carModel = car.carModel;
+            CarYear = car.CarYear;
+            Price = car.Price;
+            photoFilename = car.photoFilename;
+        }
+
+        /// <summary>
+        /// Input Constructor - Creates new Car object with input values, requires ALL values.
+        /// </summary>
+        /// <param name="regoNumber"></param>
+        /// <param name="carMake"></param>
+        /// <param name="carModel"></param>
+        /// <param name="CarYear"></param>
+        /// <param name="Price"></param>
+        /// <param name="photoFilename"></param>
+        public Car(int regoNumber, Make carMake, Model carModel, 
+            int CarYear, float Price, string photoFilename)
+        {
+            this.regoNumber = regoNumber;
+            this.carMake = carMake;
+            this.carModel = carModel;
+            this.CarYear = CarYear;
+            this.Price = Price;
+            this.photoFilename = photoFilename;
+        }
+    }
+
+    enum Make
+    {
+        White,
+        Black,
+        Grey,
+        Red,
+        Blue,
+        Yellow
+    }
+
+    enum Model
+    {
+        SUV,
+        Truck,
+        Sudan,
+        Coupe,
+        StationWagon,
+        SportsCar,
+        HatchBack,
+        Convertible
+    }
+}
---
+++ Car App/Car App/Garage.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Car_App
+{
+    class Garage
+    {
+        Car[] parkingSpots = new Car[20];
+        public List<Car> SoldCars = new List<Car>();
+
+        public Dictionary<int, Car> GetDictCarsFromLot()
+        {
+            Dictionary<int, Car> dict = new Dictionary<int, Car>();
+            foreach (Car car in parkingSpots)
+            {
+                dict.Add(Array.FindIndex(parkingSpots, spot => spot == car) + 1, car);
+            }
+            return dict;
+        }
+
+        public List<Car> GetCarsFromLot()
+        {
+            List<Car> cars = new List<Car>();
+            foreach(Car car in parkingSpots)
+            {
+                cars.Add(car);
+            }
+            return cars;
+        }
+
+        public Car GetCarBySpot(int spot)
+        {
+            return parkingSpots[spot + 1];
+        }
+
+        public List<Car> SearchByBudget(List<Car> cars, float min = 0f, float max = float.MaxValue)
+        {
+            return cars.Where(car => car.Price >= min && car.Price <= max).ToList();
+        }
+
+        public List<Car> SearchbyModel(List<Car> cars, Model model)
+        {
+            return cars.Where(car => car.CarModel == model).ToList();
+        }
+
+        public List<Car> SearchByMake(List<Car> cars, Make make)
+        {
+            return cars.Where(car => car.CarMake == make).ToList();
+        }
+
+        public Car GetCarByRego(int rego)
+        {
+            return parkingSpots.ToList<Car>().Find(car => car.RegoNumber == rego);
+        }
+
+        public bool TryGetCarByRego(int rego, out Car car)
+        {
+            car = parkingSpots.ToList<Car>().Find(c => c.RegoNumber == rego);
+            if(car != null)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public List<int> GetEmptySpots()
+        {
+            List<int> list = new List<int>();
+            for(int i = 0; i < parkingSpots.Length; i++)
+            {
+                if(parkingSpots[i] == null)
+                {
+                    list.Add(i + 1);
+                }
+            }
+            return list;
+        }
+
+    }
+}
